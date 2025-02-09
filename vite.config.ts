a16d52adbfb43a335a69212f475c7bbf791f266a
Rafael-66 <rafael.augusto.cjn@gmail.com>
2025-02-08 23:55:55 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://rafael-66.github.io/deploying-vite-project-example/",
+  base: '/pages/dist/',
 });
